02466207bd84a94e54eb36652ff1c929682e4c3b
Nikoraii <kolja.st@gmail.com>
2023-01-30 15:15:00 +0100

    Add contact form to index.php

---
+++ contact.php
@@ -0,0 +1,46 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'vendor/autoload.php';
+
+// Get the dotenv variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+$mail = new PHPMailer(true);
+
+$mail->SMTPDebug = 3;
+// Get the form data
+$data = $_POST;
+
+$name = $data['name'];
+$email = $data['email'];
+$message = $data['message'];
+
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+$mail->SMTPAuth = true;
+$mail->Username = $_ENV['C_EMAIL'];
+$mail->Password = $_ENV['C_PASS'];
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+
+$mail->setFrom($email, $name);
+$mail->addAddress('kolja.st@gmail.com');
+$mail->addReplyTo($email, $name);
+
+$mail->isHTML(true);
+$mail->Subject = 'Contact Form Submission';
+$mail->Body = "Name: $name <br> Email: $email <br> Message: $message";
+
+
+try {
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
+?>
--- index.php
+++ index.php
@@ -75,16 +75,16 @@
                     <img src="assets/images/pizza-delivery.jpg" width="100%" alt="Pizza in delivery box">
                 </div>
                 <div class="col-6 p-0 about-text">
-                    <h2 data-aos="fade-left">Lorem Ipsum</h2>
-                    <p data-aos="fade-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quos hic vero dignissimos. Voluptatibus magnam magni ut quam ipsum, vitae atque deleniti, temporibus laudantium repellat reiciendis aperiam inventore quis voluptatum?</p>
-                    <p data-aos="fade-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quos hic vero dignissimos. Voluptatibus magnam magni ut quam ipsum, vitae atque deleniti, temporibus laudantium repellat reiciendis aperiam inventore quis voluptatum?</p>
+                    <h2 data-aos="fade-left" data-aos-once="true">Lorem Ipsum</h2>
+                    <p data-aos="fade-left" data-aos-once="true">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quos hic vero dignissimos. Voluptatibus magnam magni ut quam ipsum, vitae atque deleniti, temporibus laudantium repellat reiciendis aperiam inventore quis voluptatum?</p>
+                    <p data-aos="fade-left" data-aos-once="true">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quos hic vero dignissimos. Voluptatibus magnam magni ut quam ipsum, vitae atque deleniti, temporibus laudantium repellat reiciendis aperiam inventore quis voluptatum?</p>
                 </div>
             </div>
             <div class="row row-after">
                 <div class="col-6 p-0 about-text">
-                    <h2 data-aos="fade-right">Lorem Ipsum</h2>
-                    <p data-aos="fade-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quos hic vero dignissimos. Voluptatibus magnam magni ut quam ipsum, vitae atque deleniti, temporibus laudantium repellat reiciendis aperiam inventore quis voluptatum?</p>
-                    <p data-aos="fade-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quos hic vero dignissimos. Voluptatibus magnam magni ut quam ipsum, vitae atque deleniti, temporibus laudantium repellat reiciendis aperiam inventore quis voluptatum?</p>
+                    <h2 data-aos="fade-right" data-aos-once="true">Lorem Ipsum</h2>
+                    <p data-aos="fade-right" data-aos-once="true">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quos hic vero dignissimos. Voluptatibus magnam magni ut quam ipsum, vitae atque deleniti, temporibus laudantium repellat reiciendis aperiam inventore quis voluptatum?</p>
+                    <p data-aos="fade-right" data-aos-once="true">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quos hic vero dignissimos. Voluptatibus magnam magni ut quam ipsum, vitae atque deleniti, temporibus laudantium repellat reiciendis aperiam inventore quis voluptatum?</p>
                 </div>
                 <div class="col-6 p-0">
                     <img  src="assets/images/pizza-making.jpg" width="100%" alt="Making Pizza">
@@ -97,33 +97,33 @@
         <div class="container-fluid text-right">
             <div class="row">
                 <div class="col-5">
-                    <img data-aos="fade-up" src="assets/images/pizza.png" alt="" width="auto" height="100px">
+                    <img data-aos="fade-up" data-aos-once="true" src="assets/images/pizza.png" alt="" width="auto" height="100px">
                 </div>
                 <div class="col-7 text-left mt-3">
-                    <h1 data-aos="fade-up" class="display-4">OPEN 24/7</h1>
+                    <h1 data-aos="fade-up" data-aos-once="true" class="display-4">OPEN 24/7</h1>
                 </div>
             </div>
         </div>
     </section>
 
     <section id="chefs">
-        <h2 data-aos="fade-up">Our Chefs</h2>
+        <h2 data-aos="fade-up" data-aos-once="true">Our Chefs</h2>
         <div class="container">
             <div class="row chefs-row">
                 <div class="col-4">
-                    <img  class="img img-fluid chef-picture" data-aos="fade-up" src="https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/pavoni_tcl_d.jpg?itok=m6VN2mYL" alt="">
-                    <h3 data-aos="fade-up">Walter White</h3>
-                    <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus hic non unde dolorem, aut natus officiis, recusandae provident nam exercitationem numquam optio quos vel voluptate blanditiis libero error asperiores! Ut?</p>
+                    <img  class="img img-fluid chef-picture" data-aos="fade-up" data-aos-once="true" src="https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/pavoni_tcl_d.jpg?itok=m6VN2mYL" alt="">
+                    <h3 data-aos="fade-up" data-aos-once="true">Walter White</h3>
+                    <p data-aos="fade-up" data-aos-once="true">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus hic non unde dolorem, aut natus officiis, recusandae provident nam exercitationem numquam optio quos vel voluptate blanditiis libero error asperiores! Ut?</p>
                 </div>
                 <div class="col-4">
-                    <img class="img img-fluid chef-picture" data-aos="fade-up" src="https://nerdyfoodies.com/wp-content/uploads/2021/01/chef-fabio-viviani-1024x1024.jpg" alt="">
-                    <h3 data-aos="fade-up">Jesse Pinkman</h3>
-                    <p data-aos="fade-up">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum recusandae repellat quia, deleniti fugit natus placeat blanditiis accusamus aliquid repudiandae, laborum perspiciatis nihil saepe ratione doloremque? Ex hic quia at.</p>
+                    <img class="img img-fluid chef-picture" data-aos="fade-up" data-aos-once="true" src="https://nerdyfoodies.com/wp-content/uploads/2021/01/chef-fabio-viviani-1024x1024.jpg" alt="">
+                    <h3 data-aos="fade-up" data-aos-once="true">Jesse Pinkman</h3>
+                    <p data-aos="fade-up" data-aos-once="true">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum recusandae repellat quia, deleniti fugit natus placeat blanditiis accusamus aliquid repudiandae, laborum perspiciatis nihil saepe ratione doloremque? Ex hic quia at.</p>
                 </div>
                 <div class="col-4">
-                    <img class="img img-fluid chef-picture" data-aos="fade-up" src="https://i.dailymail.co.uk/1s/2019/04/17/01/12365980-6930129-image-a-25_1555459844494.jpg" alt="">
-                    <h3 data-aos="fade-up">Mike Ehrmantraut</h3>
-                    <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolores rerum corrupti? Architecto dolorem amet tenetur inventore dolores non, dolorum aliquid praesentium ipsum quae vero distinctio rerum perspiciatis vel exercitationem.</p>
+                    <img class="img img-fluid chef-picture" data-aos="fade-up" data-aos-once="true" src="https://i.dailymail.co.uk/1s/2019/04/17/01/12365980-6930129-image-a-25_1555459844494.jpg" alt="">
+                    <h3 data-aos="fade-up" data-aos-once="true">Mike Ehrmantraut</h3>
+                    <p data-aos="fade-up" data-aos-once="true">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolores rerum corrupti? Architecto dolorem amet tenetur inventore dolores non, dolorum aliquid praesentium ipsum quae vero distinctio rerum perspiciatis vel exercitationem.</p>
                 </div>
             </div>
         </div>
@@ -133,30 +133,30 @@
         <div class="container-fluid p-3">
             <div class="row">
                 <div class="col-8">
-                    <iframe data-aos="zoom-in-right" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d104478.97862129203!2d-106.74651910968689!3d35.08253024379721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87220addd309837b%3A0xc0d3f8ceb8d9f6fd!2z0JDQu9Cx0YPQutC10YDQutC4LCDQndC-0LLQuCDQnNC10LrRgdC40LrQviwg0KHRmNC10LTQuNGa0LXQvdC1INCU0YDQttCw0LLQtQ!5e0!3m2!1ssr!2srs!4v1674831795222!5m2!1ssr!2srs" width="100%" height="650" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe data-aos="zoom-in-right" data-aos-once="true" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d104478.97862129203!2d-106.74651910968689!3d35.08253024379721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87220addd309837b%3A0xc0d3f8ceb8d9f6fd!2z0JDQu9Cx0YPQutC10YDQutC4LCDQndC-0LLQuCDQnNC10LrRgdC40LrQviwg0KHRmNC10LTQuNGa0LXQvdC1INCU0YDQttCw0LLQtQ!5e0!3m2!1ssr!2srs!4v1674831795222!5m2!1ssr!2srs" width="100%" height="650" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div class="col-4">
-                    <h4 data-aos="zoom-in-left">Find Us Here</h4>
-                    <p data-aos="zoom-in-left"><i class="fa fa-location-arrow" aria-hidden="true"></i> 65th Avenue 43</p>
-                    <h4 data-aos="zoom-in-left">Call us</h4>
-                    <p data-aos="zoom-in-left"><i class="fa fa-phone" aria-hidden="true"></i> (+381)00-000-0000</p>
-                    <div data-aos="zoom-in-left" class="form-divider"></div>
+                    <h4 data-aos="zoom-in-left" data-aos-once="true">Find Us Here</h4>
+                    <p data-aos="zoom-in-left" data-aos-once="true"><i class="fa fa-location-arrow" aria-hidden="true"></i> 65th Avenue 43</p>
+                    <h4 data-aos="zoom-in-left" data-aos-once="true">Call us</h4>
+                    <p data-aos="zoom-in-left" data-aos-once="true"><i class="fa fa-phone" aria-hidden="true"></i> (+381)00-000-0000</p>
+                    <div data-aos="zoom-in-left" data-aos-once="true" class="form-divider"></div>
                     <div class="form-section">
-                        <h4 data-aos="zoom-in-left">Email us</h4>
-                        <form action="#" method="post" id="contact-form">
+                        <h4 data-aos="zoom-in-left" data-aos-once="true">Email us</h4>
+                        <form action="#" method="post" id="contact-form" data-aos="zoom-in-left" data-aos-once="true">
                             <div class="form-group">
-                                <label data-aos="zoom-in-left" for="">Name:</label>
-                                <input data-aos="zoom-in-left" type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="John Smith...">
+                                <label  for="">Name:</label>
+                                <input  type="text" class="form-control" name="name" id="name" aria-describedby="helpId" placeholder="John Smith...">
                             </div>
                             <div class="form-group">
-                                <label data-aos="zoom-in-left" for="">Email:</label>
-                                <input data-aos="zoom-in-left" type="email" name="" id="" class="form-control" placeholder="example@gmail.com" aria-describedby="helpId">
+                                <label  for="">Email:</label>
+                                <input  type="email" name="email" id="email" class="form-control" placeholder="example@gmail.com" aria-describedby="helpId">
                             </div>
                             <div class="form-group">
-                                <label data-aos="zoom-in-left" for="">Message:</label>
-                                <textarea data-aos="zoom-in-left" class="form-control" name="" id="" rows="3"></textarea>
+                                <label  for="">Message:</label>
+                                <textarea  class="form-control" name="message" id="message" rows="3" placeholder="Your message here..."></textarea>
                             </div>
-                            <button data-aos="zoom-in-left" type="submit" class="btn">Submit</button>
+                            <button  type="submit" class="btn">Send</button>
                         </form>
                     </div>
                 </div>
@@ -180,7 +180,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="https://unpkg.com/swup@3"></script>
     <script>
         AOS.init();
         $(document).ready(function(){
@@ -211,7 +210,28 @@
             });
             $("#btn-back-to-top").click(function() {
                 window.scrollTo({ top: 0});
-            })
+            });
+            const form = document.getElementById("contact-form");
+
+            form.addEventListener("submit", event => {
+                event.preventDefault();
+
+                const formData = new FormData(form);
+
+                fetch("contact.php", {
+                    method: "POST",
+                    body: formData
+                })
+                .then(response => response.text())
+                .then(data => {
+                    console.log(data);
+                    // Perform any additional actions
+                })
+                .catch(error => {
+                    console.error("Error:", error);
+                });
+            });
+
         });
     </script>
   </body>
