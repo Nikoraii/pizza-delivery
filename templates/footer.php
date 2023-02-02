b01a357d4c41977eecd49ea05ae10d076ae744cd
Nikoraii <kolja.st@gmail.com>
2023-02-02 22:48:38 +0100

    Footer moved to templates

---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+<footer class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        &copy; <?php echo date("Y"); ?>
+        Made with <i class="fa fa-heart" aria-hidden="true"></i> by
+        <a class="text-dark" traget="" href="https://github.com/Nikoraii">Nikoraii</a>
+    </div>
+    <!-- Copyright -->
+</footer>
