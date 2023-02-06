2c253bb837d8f364f276cfcb395e2707b3684dcd
Nikoraii <kolja.st@gmail.com>
2023-02-07 00:29:21 +0100

    get drink by id

--- models/Drink.php
+++ models/Drink.php
@@ -12,4 +12,23 @@ class Drink extends Table
 
         return $db->select('Drink', $query);
     }
+
+    public static function getDrink($drink_id)
+    {
+        $db = Database::getInstance();
+
+        $query = 'SELECT * FROM products_drinks WHERE id = :drink_id';
+
+        $params = [
+            ':drink_id' => $drink_id
+        ];
+
+        $drinks = $db->select('Drink', $query, $params);
+
+        foreach ($drinks as $drink) {
+            return $drink;
+        }
+
+        return false;
+    }
 }
